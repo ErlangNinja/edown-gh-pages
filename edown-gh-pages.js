6b33422dadbe695169ac7b1e087ff7dfe42161ae
Erik Hedenström <erik@erlang.ninja>
2014-10-25 00:13:50 +0200

    Cleaned up CSS. Added on_content callback.

--- edown-gh-pages.js
+++ edown-gh-pages.js
@@ -5,56 +5,63 @@ if (!window.console) {
     };
 }
 
-var edown_gh_pages = function(options) {
+var edown_gh_pages = function (options) {
 
-	var opts = $.extend({
-		sidebar_id: "#sidebar",
-		content_id: "#content",
-		base_url: "",
-		start_page: "README.md"
-	}, options);
+    var opts = $.extend({
+        sidebar_id: "#sidebar",
+        content_id: "#content",
+        base_url: "",
+        start_page: "README.md",
+        on_load: function(element) { }
+    }, options);
 
-	function hashChange() {
+    function hashChange() {
         if (location.hash.indexOf("#!") == 0) {
             loadContent(location.hash.slice(2));
         }
-	}
+    }
 
     function loadContent(URL) {
         $.get(URL, function (markdown) {
             marked(markdown, function (err, content) {
-                if (err) throw err;
+                if (err) throw err
                 $(opts.content_id).html(content);
+                var element = $(opts.content_id).get();
+                $('pre code', element).each(function(i, block) {
+                    if (block.className.indexOf("lang-") < 0) {
+                        $(block).addClass("lang-erlang");
+                    }
+                });
+                opts.on_load(element);
             });
         });
     }
 
-	function extractModules() {
-		$(opts.sidebar_id).append("<h3>Modules</h3>");
+    function extractModules() {
+        $(opts.sidebar_id).append("<li><strong>Modules</strong></li>");
         $(opts.content_id).find("table[summary='list of modules'] a").each(function (i, module) {
-        	var label =  $(module).text();
             var href = "#!" + opts.base_url + $(module).attr("href");
             $(opts.sidebar_id).append("<li><a href=\"" + href + "\">" + $(module).text() + "</a></li>");
             $(module).attr("href", href);
         });
-	}
+    }
 
-	function loadStartPage() {
-        $.get(opts.base_url+opts.start_page, function (markdown) {
+    function loadStartPage() {
+        $.get(opts.base_url + opts.start_page, function (markdown) {
             marked(markdown, function (err, html) {
                 if (err) throw err;
-                $(opts.sidebar_id).append("<li><a href=\"#!" + opts.base_url+opts.start_page + "\">Home</a></li>");
-		        $(opts.content_id).html(html);
-		        document.title = $(opts.content_id).find("h1").text();
-		        extractModules();
+                $(opts.sidebar_id).append("<li><strong><a href=\"#!" + opts.base_url + opts.start_page + "\">Home</a></strong></li>");
+                $(opts.content_id).html(html);
+                document.title = $(opts.content_id).find("h1").text();
+                extractModules();
                 $(window).on("hashchange", hashChange);
                 hashChange();
             });
-        });		
-	}
+        });
+    }
 
     $(function () {
-    	loadStartPage();
+        loadStartPage();
     });
 
 };
